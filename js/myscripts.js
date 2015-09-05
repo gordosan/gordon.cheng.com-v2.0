cdbc13ccb2379fc654df76e1bd533cb9aff481e1
Gordon Cheng <gordosann@gmail.com>
2015-09-05 16:57:08 -0400

    major update

---
+++ js/myscripts.js
@@ -0,0 +1,62 @@
+var theToggle = document.getElementById('toggle');
+
+
+// hasClass
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// addClass
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+    	elem.className += ' ' + className;
+    }
+}
+// removeClass
+function removeClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+// toggleClass
+function toggleClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+   $("#menu_placeholder").hide();
+   return false;
+}
+
+function footer(){
+     var dteNow = new Date();
+     var intYear = dteNow.getFullYear();
+     document.write("© " + intYear +  " Gordon Cheng");  
+}
+
+$(document).ready(function(){
+  var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
+  var traX, traY;
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((5 * mouseX) / 570) + 40;
+    traY = ((5 * mouseY) / 570) + 50;
+    $(".content p").css({"background-position": traX + "%" + traY + "%"});
+    $("#headline").css({"background-position": traX + "%" + traY + "%"});
+  });
+});
